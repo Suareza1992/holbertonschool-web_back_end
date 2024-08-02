190c902afedfd573f4743b4af928d479efae2e8d
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-08-02 06:47:38 -0400

    Task 4 file.

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeRecord = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeRecord ? gradeRecord.grade : 'N/A',
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
