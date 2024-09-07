8729a490bbbacf65bc5ac0066e0e52c2429befcc
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-09-07 19:11:01 -0400

    Task 4 typo.

--- python_variable_annotations/4-main.py
+++ python_variable_annotations/4-main.py
@@ -8,4 +8,4 @@ school = __import__('4-define_variables').school
 print("a is a {} with a value of {}".format(type(a), a))
 print("pi is a {} with a value of {}".format(type(pi), pi))
 print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
-print("school is a {} with a value of {}".format(type(scgool), school))
+print("school is a {} with a value of {}".format(type(school), school))
