1b0bd35bf936eb3f0f48e9a7afdcd14128c15c8a
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-09-07 05:16:57 -0400

    Task 0 with PEP-8.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-""" 
+"""
 Type-annotated function that takes two floats, a and b,
 as arguments and returns their sum as a float.
 """
-
 
 def add(a: float, b: float) -> float:
     """ add function """
