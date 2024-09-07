3dfa3b5ccfe25e605a5c17a0af848c990407296e
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-09-07 18:00:27 -0400

    Task 3.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Type-annotated function to_str that takes a float n
+as an argument and returns the string representation
+of the float.
+"""
+
+
+def to_str(n: float) -> str;
+    """ String representation of the float."""
+    return str(n)
