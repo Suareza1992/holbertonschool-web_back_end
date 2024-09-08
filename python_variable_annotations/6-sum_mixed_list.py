6dd251fc1f2b4dd290382f3fba367c35ed79fee5
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-09-07 20:14:14 -0400

    Task 6: Correcting typo.

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -11,4 +11,4 @@ from typing import Union, List
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Sum every item in a list. """
-    return sum(mxd_list)
+    return sum(mxd_lst)
