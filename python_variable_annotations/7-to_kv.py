113412f70400f79d3fe489115b2a98adcc3279b9
Angel Suarez <angelsuarez@Angels-MacBook-Pro.local>
2024-09-07 20:39:09 -0400

    Task 7 typo correction.

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -13,7 +13,8 @@ annotated as a float.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float] -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Take a string and in OR float and return a tuple."""
+    Take a string and in OR float and return a tuple.
+    """
     return (k, v**2)
